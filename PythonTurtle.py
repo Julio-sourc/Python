25601b2daed926279cb5f010bf66b6dc4a41f459
Julio-sourc <j.julio@uni9.edu.br>
2021-09-05 21:49:07 -0300

    turtle

---
+++ PythonTurtle.py
@@ -0,0 +1,14 @@
+from turtle import *
+
+shape("turtle")#coloca o formato da tartaruga
+bgcolor("blue")#coloca cor na janela
+
+delay(50)#atrasar 50 milissegundos
+forward(100) #anda para frente 200 pixels
+right(90) #vira 90 graus para adireita
+forward(200) #anda para frente 200 pixels
+right(90) #vira 90 graus para adireita
+forward(100) #anda para frente 200 pixels
+right(90) #vira 90 graus para adireita
+forward(200) #anda para frente 200 pixels
+input("pressione enter para sair...")
